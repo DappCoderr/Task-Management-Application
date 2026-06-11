92553406e92a7331f182dad568e3d18e6d24078c
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-06-11 15:25:42 +0530

    setup user and task model using sequelize ORM

---
+++ src/model/Task.js
@@ -0,0 +1,40 @@
+import { sequelize } from "../config/database";
+import { DataTypes } from "sequelize";
+
+export const Task = sequelize.define("Task", 
+    {
+        id:{
+            type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.ENUM("TODO", "IN PROGRESS", "COMPLETED"),
+            defaultValue: "TODO"
+        },
+        priority: {
+            type: DataTypes.ENUM("LOW", "MEDIUM", "HIGH"),
+            defaultValue: "LOW"
+        },
+        dueDate: {
+            type: DataTypes.DATE
+        },
+        userId: {
+            type: DataTypes.UUID,
+            references: {
+                model: "users",
+                key: "id"
+            }
+        },
+    }, {
+    tableName: "tasks",
+    timestamps: true,
+})
